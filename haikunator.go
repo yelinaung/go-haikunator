55bce976744265b580da2503695046b188a4602b
Pieter Joost van de Sande <pj@born2code.net>
2021-01-20 00:18:55 +0100

    feat: increase number of adjectives and nouns

--- haikunator.go
+++ haikunator.go
@@ -6,22 +6,30 @@ import (
 )
 
 var (
-	ADJECTIVES = []string{"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer",
-		"icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient",
-		"twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing",
-		"broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering",
-		"bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small",
-		"sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black",
-		"young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral",
-		"restless", "divine", "polished", "ancient", "purple", "lively", "nameless"}
-	NOUNS = []string{"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning",
-		"snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest",
-		"hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly",
-		"bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass",
-		"haze", "mountain", "night", "pond", "darkness", "snowflake", "silence",
-		"sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower",
-		"wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog",
-		"frost", "voice", "paper", "frog", "smoke", "star"}
+	ADJECTIVES = []string{"aged", "ancient", "autumn", "billowing", "bitter", "black", "blue", "bold",
+  "broad", "broken", "calm", "cold", "cool", "crimson", "curly", "damp",
+  "dark", "dawn", "delicate", "divine", "dry", "empty", "falling", "fancy",
+  "flat", "floral", "fragrant", "frosty", "gentle", "green", "hidden", "holy",
+  "icy", "jolly", "late", "lingering", "little", "lively", "long", "lucky",
+  "misty", "morning", "muddy", "mute", "nameless", "noisy", "odd", "old",
+  "orange", "patient", "plain", "polished", "proud", "purple", "quiet", "rapid",
+  "raspy", "red", "restless", "rough", "round", "royal", "shiny", "shrill",
+  "shy", "silent", "small", "snowy", "soft", "solitary", "sparkling", "spring",
+  "square", "steep", "still", "summer", "super", "sweet", "throbbing", "tight",
+  "tiny", "twilight", "wandering", "weathered", "white", "wild", "winter", "wispy",
+  "withered", "yellow", "young"}
+	NOUNS = []string{"art", "band", "bar", "base", "bird", "block", "boat", "bonus",
+  "bread", "breeze", "brook", "bush", "butterfly", "cake", "cell", "cherry",
+  "cloud", "credit", "darkness", "dawn", "dew", "disk", "dream", "dust",
+  "feather", "field", "fire", "firefly", "flower", "fog", "forest", "frog",
+  "frost", "glade", "glitter", "grass", "hall", "hat", "haze", "heart",
+  "hill", "king", "lab", "lake", "leaf", "limit", "math", "meadow",
+  "mode", "moon", "morning", "mountain", "mouse", "mud", "night", "paper",
+  "pine", "poetry", "pond", "queen", "rain", "recipe", "resonance", "rice",
+  "river", "salad", "scene", "sea", "shadow", "shape", "silence", "sky",
+  "smoke", "snow", "snowflake", "sound", "star", "sun", "sun", "sunset",
+  "surf", "term", "thunder", "tooth", "tree", "truth", "union", "unit",
+  "violet", "voice", "water", "waterfall", "wave", "wildflower", "wind", "wood"}
 )
 
 type Name interface {
